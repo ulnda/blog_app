7370c165d498e9c7ebd57c5047ac7e1727a213cc
ulnda <denis.nagibin@gmail.com>
2014-12-12 20:47:00 +0300

    Sessions controller RSpec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,21 +2,29 @@
 
 require 'spec_helper'
 
-describe 'Sessions' do
+describe SessionsController do
+  render_views
+
   USER_PASSWORD = '12345678'
 
   let!(:user) { create(:user, password: USER_PASSWORD) }
 
-  describe 'Create' do
-    context 'with valid data' do
-    	before { post user_session_path, user: { email: user.email, password: USER_PASSWORD } }
+  describe '#create' do
+    before { request.env['devise.mapping'] = Devise.mappings[:user] }
+
+    context 'with valid user data' do
+    	before do 
+        post :create, user: { email: user.email, password: USER_PASSWORD }, format: :json
+      end
 
     	it { expect(response.status).to eq(200) }	
       it { expect(JSON.parse(response.body)['name']).to eq(user.name) }
     end
 
     context 'with invalid data' do
-      before { post user_session_path, user: { email: user.email, password: USER_PASSWORD + '#' } }
+      before do 
+        post :create, user: { email: user.email, password: USER_PASSWORD + '#' }, format: :json
+      end
 
       it { expect(response.status).to eq(401) } 
       it { expect(JSON.parse(response.body)['errors'].size).to eq(1) }
